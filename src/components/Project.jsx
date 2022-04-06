851e82301bac54d5fb5fc9b5601f4029129f1e35
Chloe <chloe_g@btinternet.com>
2022-04-06 14:33:00 +0100

    Add individual project component

---
+++ src/components/Project.jsx
@@ -0,0 +1,30 @@
+import { Typography, IconButton, Box } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LaunchIcon from "@mui/icons-material/Launch";
+
+export default function Project({ project }) {
+  return (
+    <>
+      <Box sx={{ textAlign: "justify", mt: 3, mb: 3 }}>
+        <Typography variant="p">{project.description}</Typography>
+      </Box>
+      <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+        {project.technologies.map((technology) => {
+          return (
+            <Typography variant="overline" component="p" sx={{ mr: 2 }} >
+              {technology}
+            </Typography>
+          );
+        })}
+      </Box>
+      <IconButton aria-label="view GitHub repository" size="large" href={project.gitHubUrl} color="primary">
+        <GitHubIcon fontSize="inherit" />
+      </IconButton>
+      {project.liveUrl && (
+        <IconButton aria-label="view live project" size="large" href={project.liveUrl} color="primary">
+          <LaunchIcon fontSize="inherit" />
+        </IconButton>
+      )}
+    </>
+  );
+}
