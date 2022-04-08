e783a5e672413aac6112afe95fec0bbb965a8059
Chloe <chloe_g@btinternet.com>
2022-04-08 10:16:20 +0100

    update to v18 React syntax

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 import App from "./App";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -7,11 +7,11 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"))
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
 );
